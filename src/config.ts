ed72907bbd5685407a1477ca615944a1c27a6d87
Suryansh <suryansh02973@gmail.com>
2026-05-03 23:00:52 +0530

    music change

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Suryansh",
   profile: "https://github.com/thatboringcoder",
   desc: "A space where curiosity becomes code. Exploring web development, software architecture and everything that makes the tech world spin.",
-  title: "Astro Devosfera",
+  title: "Hi!, I'm Suryansh",
   ogImage: "devosfera-og.webp", // located in the public folder
   lightAndDarkMode: true,
   postPerIndex: 6,
@@ -19,7 +19,7 @@ export const SITE = {
     suffix: "$", // terminal symbol on the right
   },
   backdropEffects: {
-    cursorGlow: false, // cursor tracking with soft halo
+    cursorGlow: true, // cursor tracking with soft halo
     grain: true, // background visual noise layer
   },
   editPost: {
@@ -30,11 +30,11 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "America/Guatemala", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Kolkata", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
   introAudio: {
     enabled: true, // show/hide intro player in home and compact player while navigating
     // src: path to file (relative to /public or absolute URL). Example: "/intro.mp3" or "https://example.com/stream"
-    src: "https://fluxfm.streamabc.net/flx-chillhop-mp3-128-8581707",
+    src: "https://www.epidemicsound.com/music/tracks/877a365c-5acd-4e9f-a41d-ebb674dc2464/",
     // src: "/audio/intro-web.mp3",
     isStream: true, // true for radio/live stream URLs (example: https://fluxfm.streamabc.net/flx-chillhop-mp3-128-8581707)
     label: "LOFI", // display label in player
